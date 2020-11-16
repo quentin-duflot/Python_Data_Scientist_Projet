2631a5a0567cefbe7322e4b472f91be12bbb96df
quentinferon <quentinferon@users.noreply.github.com>
2020-11-17 00:49:53 +0100

    Update Gestion de portefeuille.py

--- Gestion de portefeuille.py
+++ Gestion de portefeuille.py
@@ -52,9 +52,124 @@ for x in name_cac40 :
 data_cac40.keys()
 
 
+# # Diagramme à bougies
+
+# In[8]
+
+def pandas_candlestick_ohlc(dat, stick = "day", otherseries = None):
+    """
+    :param dat: pandas DataFrame object with datetime64 index, and float columns "Open", "High", "Low", and "Close", likely created via DataReader \
+    from "yahoo"
+    :param dat: un DataFrame de pandas avec une date type datetime64 et les colonnes de floats suivantes : "Open", "High", "Low", and "Close", \
+    le plus souvent créées à partir du datareader de "yahoo"
+
+    :param stick: A string or number indicating the period of time covered by a single candlestick. Valid string inputs include "day", "week", \
+    "month", and "year", ("day" default), and any numeric input indicates the number of trading days included in a period
+    :param stick: une chaine de caractères ou un nombre pour la période de temps couverte par une seule bougie. Les seules chaines de caractères \
+    possibles sont  "day", "week", "month", et "year", ("day" par défault), et tous les autres floats qui indiquent le nombre de trades dans la \
+    période.
+
+    :param otherseries: An iterable that will be coerced into a list, containing the columns of dat that hold other series to be plotted as lines
+    :param otherseries: un itérable de type liste qui contient les colonnes de dat pour les autres séries à plotter en lignes
+
+    This will show a Japanese candlestick plot for stock data stored in dat, also plotting other series if passed.
+    
+    Sort un graphique en bougie pour les dates stockées dans dat. 
+    """
+    mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+    alldays = DayLocator()              # minor ticks on the days
+    dayFormatter = DateFormatter('%d')      # e.g., 12
+
+    # Create a new DataFrame which includes OHLC data for each period specified by stick input
+    transdat = dat.loc[:,["Open", "High", "Low", "Close"]]
+    if (type(stick) == str):
+        if stick == "day":
+            plotdat = transdat
+            stick = 1 # Used for plotting
+        elif stick in ["week", "month", "year"]:
+            if stick == "week":
+                transdat["week"] = pd.to_datetime(transdat.index).map(lambda x: x.isocalendar()[1]) # Identify weeks
+            elif stick == "month":
+                transdat["month"] = pd.to_datetime(transdat.index).map(lambda x: x.month) # Identify months
+            transdat["year"] = pd.to_datetime(transdat.index).map(lambda x: x.isocalendar()[0]) # Identify years
+            grouped = transdat.groupby(list(set(["year",stick]))) # Group by year and other appropriate variable
+            plotdat = pd.DataFrame({"Open": [], "High": [], "Low": [], "Close": []}) # Create empty data frame containing what will be plotted
+            for name, group in grouped:
+                plotdat = plotdat.append(pd.DataFrame({"Open": group.iloc[0,0],
+                                            "High": max(group.High),
+                                            "Low": min(group.Low),
+                                            "Close": group.iloc[-1,3]},
+                                           index = [group.index[0]]))
+            if stick == "week": stick = 5
+            elif stick == "month": stick = 30
+            elif stick == "year": stick = 365
+
+    elif (type(stick) == int and stick >= 1):
+        transdat["stick"] = [np.floor(i / stick) for i in range(len(transdat.index))]
+        grouped = transdat.groupby("stick")
+        plotdat = pd.DataFrame({"Open": [], "High": [], "Low": [], "Close": []}) # Create empty data frame containing what will be plotted
+        for name, group in grouped:
+            plotdat = plotdat.append(pd.DataFrame({"Open": group.iloc[0,0],
+                                        "High": max(group.High),
+                                        "Low": min(group.Low),
+                                        "Close": group.iloc[-1,3]},
+                                       index = [group.index[0]]))
+
+    else:
+        raise ValueError('Valid inputs to argument "stick" include the strings "day", "week", "month", "year", or a positive integer')
+
+
+    # Set plot parameters, including the axis object ax used for plotting
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom=0.2)
+    if plotdat.index[-1] - plotdat.index[0] < pd.Timedelta('730 days'):
+        weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
+        ax.xaxis.set_major_locator(mondays)
+        ax.xaxis.set_minor_locator(alldays)
+    else:
+        weekFormatter = DateFormatter('%b %d, %Y')
+    ax.xaxis.set_major_formatter(weekFormatter)
+
+    ax.grid(True)
+
+    # Création du graphique en chandelier
+    candlestick_ohlc(ax, list(zip(list(date2num(plotdat.index.tolist())), plotdat["Open"].tolist(), plotdat["High"].tolist(),
+                      plotdat["Low"].tolist(), plotdat["Close"].tolist())),
+                      colorup = "black", colordown = "red", width = stick * .4)
+
+    # Plot other series (such as moving averages) as lines
+    if otherseries != None:
+        if type(otherseries) != list:
+            otherseries = [otherseries]
+        dat.loc[:,otherseries].plot(ax = ax, lw = 1.3, grid = True)
+
+    ax.xaxis_date()
+    ax.autoscale_view()
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
+    plt.show()
+    
+# In[9]
+
+def candlestick(ticker, start, end):
+    
+    df_ohlc = pdr.get_data_yahoo(ticker, start, end)[['Open','High','Low','Close']]
+    df_ohlc.reset_index(inplace=True)
+
+    fig = go.Figure(data=[go.Candlestick(x=df_ohlc['Date'],
+                    open=df_ohlc['Open'],
+                    high=df_ohlc['High'],
+                    low=df_ohlc['Low'],
+                    close=df_ohlc['Close'])])
+    fig.show()
+    
+# In[10]
+
+candlestick('BNP.PA', start, end)
+    
 # # Problem du Stock Split et Dividendes
 
-# In[8]:
+# In[11]:
 
 
 def ohlc_adj(dat):
@@ -76,14 +191,14 @@ def ohlc_adj(dat):
 
 # ## Calcul des moyennes mobiles
 
-# In[9]:
+# In[12]:
 
 
 lvmh_adj = ohlc_adj(data_cac40["lvmh"])
 lvmh_adj.head()
 
 
-# In[10]:
+# In[13]:
 
 
 lvmh_adj["20d"] = lvmh_adj["Close"].rolling(window = 20, center = False).mean()
@@ -94,7 +209,7 @@ lvmh_adj.dropna(inplace=True)
 
 # # Trend Following Strategy
 
-# In[11]:
+# In[14]:
 
 
 lvmh_adj['20d-50d'] = lvmh_adj['20d'] - lvmh_adj['50d']
@@ -105,7 +220,7 @@ lvmh_adj["Regime"] = np.where(lvmh_adj['20d-50d'] < 0, -1, lvmh_adj["Regime"])
 lvmh_adj["Regime"].value_counts()
 
 
-# In[12]:
+# In[15]:
 
 
 lvmh_adj["Regime"][-1] = 0
@@ -114,7 +229,7 @@ lvmh_adj.dropna(inplace=True)
 lvmh_adj['Signal'].plot(figsize=(9,4))
 
 
-# In[13]:
+# In[16]:
 
 
 # on créé un tableau avec les opérations qui sont faites
@@ -134,7 +249,7 @@ if lvmh_adj_signals["Signal"][0] == "Sell" :
 lvmh_adj_signals.head()
 
 
-# In[14]:
+# In[17]:
 
 
 lvmh_adj_long_profits = pd.DataFrame({
@@ -152,13 +267,13 @@ lvmh_adj_long_profits = pd.DataFrame({
 lvmh_adj_long_profits.head()
 
 
-# In[15]:
+# In[18]:
 
 
 lvmh_adj_long_profits['Profit'].sum()
 
 
-# In[16]:
+# In[19]:
 
 
 tradeperiods = pd.DataFrame({"Start": lvmh_adj_long_profits.index,
@@ -169,7 +284,7 @@ lvmh_adj_long_profits["Low"] = tradeperiods.apply(lambda x: np.min(lvmh_adj.loc[
 lvmh_adj_long_profits.head()
 
 
-# In[ ]:
+# In[20]:
 
 
 
