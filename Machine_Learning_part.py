397f91d871b06e8a83f4d9c8a0408b314c0581aa
MaximeD582 <72375585+MaximeD582@users.noreply.github.com>
2020-11-21 15:24:07 +0100

    Update Machine_Learning_part.py

--- Machine_Learning_part.py
+++ Machine_Learning_part.py
@@ -5,3 +5,13 @@
 #3) On prépare les features X, en séparant les données. Une partie pour l'entrainement (70%) et l'autre pour le test (30%)
 
 #4) On applique un Random Forest Classifier sur nos données, et on visualise les résultats : Accuracy, Matrice de Confusion, et répartition de l'importance de chaque feature.
+
+
+### importation des modules nécessaires
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import seaborn as sns
+import sys
+from random import uniform
