8ce20416a3b1db9364d187c4742248a508d4db7b
MaximeD582 <72375585+MaximeD582@users.noreply.github.com>
2020-11-21 16:22:32 +0100

    Update Corrélation_Famille_Actions.py

--- Corrélation_Famille_Actions.py
+++ Corrélation_Famille_Actions.py
@@ -10,3 +10,38 @@ plt.yticks(range(stocks_cac40.shape[1]), stocks_cac40.columns, fontsize=9.5)
 cb = plt.colorbar()
 cb.ax.tick_params(labelsize=12)
 plt.title('Matrice de corrélation', fontsize=30)
+
+cac ={x : clf for x in name_cac40}
+
+
+# On crée deux matrices de corrélation 
+# L'une nous sert à sélectionner les différentes actions selon leur corrélation
+mat_corr = stocks_cac40.corr().copy()
+
+# L'autre est immuable, et permet de ne sélectionner qu'une seule et unique fois chaque action
+mat_corr_immu = stocks_cac40.corr().copy()
+
+
+family = [[] for k in range(41)]
+
+
+liste_actions = []
+for corr in mat_corr:
+  liste_actions.append(corr)
+liste_actions
+
+
+S = 41
+
+liste_index = {k:k for k in range(0,S)}
+
+for num_ligne in range(0,S):
+  if not num_ligne in liste_index:
+    continue 
+  for num_colonne in range(0,S):
+    if not num_colonne in liste_index:
+      continue
+    if abs(mat_corr_immu.iloc[num_ligne, num_colonne]) > 0.8 :
+      family[num_ligne].append(liste_actions[num_colonne])
+      mat_corr.drop([liste_actions[num_colonne]], axis = 'columns', inplace = True)
+      del liste_index[num_colonne]
