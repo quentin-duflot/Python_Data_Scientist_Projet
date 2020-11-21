251cb17061bf6c64543d7f12eac7e0f957443d63
MaximeD582 <72375585+MaximeD582@users.noreply.github.com>
2020-11-21 15:23:09 +0100

    Update Machine_Learning_part.py

---
+++ Machine_Learning_part.py
@@ -0,0 +1,7 @@
+#1) Calculer des signaux d'achat que l'on juge correct pour servir de modèle pour un futur algorithme de trading : Préparation Feature Y
+
+#2) Pas assez de données sur les valeurs Achat/Vente comparé au nombre de données sur la valeur Conserver. On fait de la Data Augmentation pour avoir les valeurs en nombre similaire.
+
+#3) On prépare les features X, en séparant les données. Une partie pour l'entrainement (70%) et l'autre pour le test (30%)
+
+#4) On applique un Random Forest Classifier sur nos données, et on visualise les résultats : Accuracy, Matrice de Confusion, et répartition de l'importance de chaque feature.
