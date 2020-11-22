f727ae2036f0d988e21a9685e7a3ae8336e475ac
MaximeD582 <72375585+MaximeD582@users.noreply.github.com>
2020-11-22 18:51:47 +0100

    Create APT_modèle.py

---
+++ APT_modèle.py
@@ -0,0 +1,3 @@
+# télécharger les données sur ce site : http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html#International
+
+# Données journalières qui renseigne de l'actualité macroéconomique dans plusieurs région du monde
