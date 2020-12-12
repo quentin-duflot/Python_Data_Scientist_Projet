0fce064a33d2ac21914fc6e205b1b719fa9116bb
quentinferon <57185645+quentinferon@users.noreply.github.com>
2020-12-12 19:48:02 +0100

    Update APT_modèle.py

--- APT_modèle.py
+++ APT_modèle.py
@@ -1,7 +1,7 @@
 # télécharger les données sur ce site : http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html#International
 # Données journalières qui renseigne de l'actualité macroéconomique dans plusieurs région du monde
 
-"On importe les modules nécessaires au web scraping 
+"On importe les modules nécessaires au web scraping" 
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
@@ -21,3 +21,34 @@ link = target['href']
 download_url = 'http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/' + link
 urllib.request.urlretrieve(download_url,'Europe_5_Factors_Daily_CSV.zip' )
 
+#Créer une fonction qui retourne les coefficients de la régression linéaire simple et multiples de chaque facteur sur l'action étudié 
+"On importe les modules nécessaires aux régressions linéaires"
+from sklearn.linear_model import LinearRegression
+
+def CoeffReg(X,Y) :
+    beta = []
+    modeleReg=LinearRegression()
+    for i in range(len(Y)) :
+        modeleReg.fit(X,Y[i])
+        beta += modeleReg.coef
+    modeleReg.fit(X,Y)
+    BETA = modeleReg.coef
+    return beta, BETA
+
+#Créer une fonction qui calcule la valeur attendue de l'action avec la formule de l'APT.
+
+def APT(d_start,m_start,y_start,d_end,m_end,y_end,action,PARAM):
+    
+    Rf = 0 #Peut-être modifié, correspond au taux du livret A
+    DateBA = str(y_start+"-"+m_start+"-"+d_start)
+    DateBP = str(y_start+m_start+d_start)
+    DateEA = str(y_end+"-"+m_end+"-"+d_end)
+    DateEP = str(y_end+m_end+d_end)
+    X = df[df["Date"] in [DateBA:DateEA]] #df est à remplacer par le nom que l'on a donné au dataframe
+    Y = df2[df2["Date"] in [DateBP:DateEP]] #df2 est à remplacer et Date est le nom à donner à la colonne du tableur CSV
+    
+    beta,BETA = CoeffReg(X, Y)
+    ER = Rf + beta*BETA
+    
+    return ER
+    
